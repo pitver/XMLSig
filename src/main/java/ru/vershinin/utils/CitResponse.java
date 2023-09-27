36edd843a3514ebb7b88451b4321334117b81a46
pitver <pitver2014@yandex.ru>
2023-09-27 19:24:20 +0300

    work version

--- src/main/java/ru/vershinin/utils/CitResponse.java
+++ src/main/java/ru/vershinin/utils/CitResponse.java
@@ -22,57 +22,59 @@ public class CitResponse {
     public void setDATA(ResponseData DATA) {
         this.DATA = DATA;
     }
+
+    @XStreamAlias("SYSTEM")
+    public static class SystemData {
+        private Err ERR;
+
+        public Err getERR() {
+            return ERR;
+        }
+
+        public void setERR(Err ERR) {
+            this.ERR = ERR;
+        }
+    }
+
+    @XStreamAlias("ERR")
+    public static class Err {
+        @XStreamAlias("Value")
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+
+    @XStreamAlias("DATA")
+    public static class ResponseData {
+        private Body BODY;
+
+        public Body getBODY() {
+            return BODY;
+        }
+
+        public void setBODY(Body BODY) {
+            this.BODY = BODY;
+        }
+    }
+
+    @XStreamAlias("BODY")
+    public static class Body {
+        private String messageId;
+
+        public String getMessageId() {
+            return messageId;
+        }
+
+        public void setMessageId(String messageId) {
+            this.messageId = messageId;
+        }
+    }
+
 }
 
-@XStreamAlias("SYSTEM")
-class SystemData {
-    private Err ERR;
-
-    public Err getERR() {
-        return ERR;
-    }
-
-    public void setERR(Err ERR) {
-        this.ERR = ERR;
-    }
-}
-
-@XStreamAlias("ERR")
-class Err {
-    @XStreamAlias("Value")
-    private String value;
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-}
-
-@XStreamAlias("DATA")
-class ResponseData {
-    private Body BODY;
-
-    public Body getBODY() {
-        return BODY;
-    }
-
-    public void setBODY(Body BODY) {
-        this.BODY = BODY;
-    }
-}
-
-@XStreamAlias("BODY")
-class Body {
-    private String messageId;
-
-    public String getMessageId() {
-        return messageId;
-    }
-
-    public void setMessageId(String messageId) {
-        this.messageId = messageId;
-    }
-}
